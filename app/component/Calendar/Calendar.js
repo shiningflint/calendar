5cbf959cb91c1e73ee41e6111b4658b32994f1e8
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-11-01 18:47:24 +0900

    moving the calendar state up to the calendar page

--- app/component/Calendar/Calendar.js
+++ app/component/Calendar/Calendar.js
@@ -26,87 +26,41 @@ function CalendarDays(props) {
   );
 }
 
-class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentMoment: moment(),
-      selectedDay: "",
-    };
-    this.selectDate = this.selectDate.bind(this);
-    this.setNextMonth = this.setNextMonth.bind(this);
-    this.setPrevMonth = this.setPrevMonth.bind(this);
-    this.updateCalendar = this.updateCalendar.bind(this);
-  }
-
-  selectDate(day, e) {
-    if (
-      this.state.currentMoment.isBefore(day, 'month') ||
-      this.state.currentMoment.isAfter(day, 'month')
-    ) {
-      return this.updateCalendar(day);
-    }
-    this.setState({
-      selectedDay: day.format("YYYY-MM-DD"),
-    });
-  }
-
-  updateCalendar(newDate) {
-    this.setState({
-      currentMoment: newDate,
-      selectedDay: newDate.format("YYYY-MM-DD"),
-    });
-  }
-
-  setNextMonth(e) {
-    this.setState((prevState) => ({
-      currentMoment: prevState.currentMoment.add(1, 'months'),
-    }));
-  }
-
-  setPrevMonth(e) {
-    this.setState((prevState) => ({
-      currentMoment: prevState.currentMoment.subtract(1, 'months'),
-    }));
-  }
-
-  render() {
-    let currentObjects = createDateObjects(this.state.currentMoment);
-
-    return(
-      <div>
-        <div className="calendar-month-wrap">
-          <div className="calendar-arrow-left">
-            <img
-              src={require('../../assets/arrow.svg')}
-              className="calendar-arrow-left__img"
-              onClick={this.setPrevMonth}/>
-          </div>
-          <div className="calendar-arrow-right">
-            <img
-              src={require('../../assets/arrow.svg')}
-              className="calendar-arrow-right__img"
-              onClick={this.setNextMonth}/>
-          </div>
-          <div className="calendar-month-name">
-            {this.state.currentMoment.format("MMMM YYYY")}
-          </div>
+function Calendar(props) {
+  let currentObjects = createDateObjects(props.currentMoment);
+  return(
+    <div>
+      <div className="calendar-month-wrap">
+        <div className="calendar-arrow-left">
+          <img
+            src={require('../../assets/arrow.svg')}
+            className="calendar-arrow-left__img"
+            onClick={props.setPrevMonth}/>
         </div>
-        <div className="calendar-days-label">
-          {Days.map((label, i) => (
-            <div
-              key={label}
-              className="calendar-days-item">{label}</div>
-          ))}
+        <div className="calendar-arrow-right">
+          <img
+            src={require('../../assets/arrow.svg')}
+            className="calendar-arrow-right__img"
+            onClick={props.setNextMonth}/>
         </div>
-        <CalendarDays
-          monthClass="current"
-          currentObjects={currentObjects}
-          selectedDay={this.state.selectedDay}
-          selectDate={this.selectDate}/>
+        <div className="calendar-month-name">
+          {props.currentMoment.format("MMMM YYYY")}
+        </div>
       </div>
-    )
-  }
+      <div className="calendar-days-label">
+        {Days.map((label, i) => (
+          <div
+            key={label}
+            className="calendar-days-item">{label}</div>
+        ))}
+      </div>
+      <CalendarDays
+        monthClass="current"
+        currentObjects={currentObjects}
+        selectedDay={props.selectedDay}
+        selectDate={props.selectDate}/>
+    </div>
+  )
 }
 
 export default Calendar;
--- app/component/Calendar/CalendarPage.js
+++ app/component/Calendar/CalendarPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import CalendarToggle from './CalendarToggle';
 import Calendar from './Calendar';
 import TimePicker from './TimePicker';
@@ -8,18 +9,60 @@ class CalendarPage extends React.Component {
     super(props);
     this.state = {
       toggle: "calendar",
+      currentMoment: moment(),
+      selectedDay: moment()
     };
     this.handleToggle = this.handleToggle.bind(this);
+    this.selectDate = this.selectDate.bind(this);
+    this.setNextMonth = this.setNextMonth.bind(this);
+    this.setPrevMonth = this.setPrevMonth.bind(this);
+    this.updateCalendar = this.updateCalendar.bind(this);
   }
 
   handleToggle(e) {
     this.setState({
-      toggle: e
+      toggle: e,
     });
   }
 
+  selectDate(day, e) {
+    if (
+      this.state.currentMoment.isBefore(day, 'month') ||
+      this.state.currentMoment.isAfter(day, 'month')
+    ) {
+      return this.updateCalendar(day);
+    }
+    this.setState({
+      selectedDay: day.format("YYYY-MM-DD"),
+    });
+  }
+
+  updateCalendar(newDate) {
+    this.setState({
+      currentMoment: newDate,
+      selectedDay: newDate.format("YYYY-MM-DD"),
+    });
+  }
+
+  setNextMonth(e) {
+    this.setState((prevState) => ({
+      currentMoment: prevState.currentMoment.add(1, 'months'),
+    }));
+  }
+
+  setPrevMonth(e) {
+    this.setState((prevState) => ({
+      currentMoment: prevState.currentMoment.subtract(1, 'months'),
+    }));
+  }
+
   render() {
-    let content = <Calendar/>;
+    let content = <Calendar
+      currentMoment={this.state.currentMoment}
+      selectedDay={this.state.selectedDay}
+      selectDate={this.selectDate}
+      setPrevMonth={this.setPrevMonth}
+      setNextMonth={this.setNextMonth}/>;
     if (this.state.toggle === "time") {
       content = <TimePicker
         maxHour={23}
